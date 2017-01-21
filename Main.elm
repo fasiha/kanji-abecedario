f66b773a573aea2694e8d413f245d6784597fe94
Ahmed Fasih <wuzzyview@gmail.com>
2017-01-20 23:34:14 -0500

    Getting firstNoDeps

--- Main.elm
+++ Main.elm
@@ -2,6 +2,8 @@ port module Main exposing (..)
 
 import Html exposing (Html, button, div, text)
 import Html.Events exposing (onClick)
+import Http
+import Json.Decode as Decode
 
 
 main : Program Never Model Msg
@@ -14,12 +16,12 @@ main =
 
 
 type alias Model =
-    Int
+    { num : Int, target : String }
 
 
 init : ( Model, Cmd Msg )
 init =
-    ( 0, Cmd.none )
+    ( Model 0 "", Cmd.none )
 
 
 
@@ -30,6 +32,8 @@ type Msg
     = Increment
     | Decrement
     | Login
+    | AskFirstNoDeps
+    | FirstNoDeps (Result Http.Error String)
 
 
 port login : String -> Cmd msg
@@ -39,13 +43,32 @@ update : Msg -> Model -> ( Model, Cmd Msg )
 update msg model =
     case msg of
         Increment ->
-            ( model + 1, Cmd.none )
+            ( { model | num = model.num + 1 }, Cmd.none )
 
         Decrement ->
-            ( model - 1, Cmd.none )
+            ( { model | num = model.num - 1 }, Cmd.none )
 
         Login ->
             ( model, login "doesntmatter" )
+
+        AskFirstNoDeps ->
+            ( model, askFirstNoDeps )
+
+        FirstNoDeps (Ok target) ->
+            ( { model | target = target }, Cmd.none )
+
+        FirstNoDeps (Err err) ->
+            ( { model | target = (toString err) }, Cmd.none )
+
+
+firstNoDepsDecoder : Decode.Decoder String
+firstNoDepsDecoder =
+    Decode.at [ "0", "target" ] Decode.string
+
+
+askFirstNoDeps : Cmd Msg
+askFirstNoDeps =
+    Http.send FirstNoDeps (Http.get "http://localhost:3000/firstNoDeps" firstNoDepsDecoder)
 
 
 
@@ -65,7 +88,9 @@ view : Model -> Html Msg
 view model =
     div []
         [ button [ onClick Login ] [ text "Login from Elm" ]
+        , button [ onClick AskFirstNoDeps ] [ text "Ask for first target" ]
         , button [ onClick Decrement ] [ text "-" ]
         , div [] [ text (toString model) ]
         , button [ onClick Increment ] [ text "+" ]
+        , text model.target
         ]
