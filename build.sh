a428cdb66af253665e4fffe7029194615bcc9783
Ahmed Fasih <wuzzyview@gmail.com>
2017-01-24 23:07:50 -0500

    Build a dev HTML to sidestep slow uglify

--- build.sh
+++ build.sh
@@ -1,2 +1,3 @@
-elm-make Main.elm --output=public/main.js && yarn run min
-
+elm-make Main.elm --output=public/main.js
+sed -e "s:main.min.js:main.js:" public/index.html > public/dev.html
+yarn run min
