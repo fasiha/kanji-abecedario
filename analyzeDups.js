1807eaa365378a3f5ca6a8a4129c34e58dbc65b9
Ahmed Fasih <wuzzyview@gmail.com>
2017-02-06 00:50:06 -0500

    Help and Export analysis

--- analyzeDups.js
+++ analyzeDups.js
@@ -1,5 +1,6 @@
 var fs = require('fs');
 var bases = JSON.parse(fs.readFileSync('data/svgs.json', 'utf8')).heading2base;
+var svgs = JSON.parse(fs.readFileSync('data/svgs.json', 'utf8')).columns;
 
 var a = Object.values(bases).reduce((p, c) => p.concat(c));
 a.length;
@@ -21,6 +22,33 @@ Object.values(_.groupBy(a))
     .filter(v => v.length > 1)
     .map(v => v[0])
     .map(s => o[s]);
+
+var flatten1 = v => v.reduce((prev, curr) => prev.concat(curr), []);
+var dict = flatten1(_.map(bases, (vs, k) => vs.map((v, i) => [v, k, i + 1])));
+var reps = _.filter(_.groupBy(dict, o => o[0]), (v, k) => v.length > 1);
+
+var base2num = new Map(Object.keys(bases).map((o, i) => [o, i]));
+var sortedReps =
+    _.sortBy(reps, v => Math.min(...v.map(vv => base2num.get(vv[1]))));
+
+var table = sortedReps.map(
+    v => flatten1([ v[0][0], v.map(v => v.slice(1).join('').toUpperCase()) ]));
+
+var repStrings = sortedReps.map(
+    v => [v[0][0], v.map(v => v.slice(1).join('').toUpperCase()).join(', ')]
+             .join(': '));
+console.log(repStrings.map(s => `<li>${s}</li>`).join('\n'));
+
+var engs = _.sortBy(dict.filter(([ s ]) => s.match(/[a-zA-Z]/)),
+                    v => base2num.get(v[1]));
+var engSvgs = engs.map(v => svgs[v[1]][v[2] - 1]);
+var engStrings = engs.map(([ key, a, n ]) => `${key} = ${a.toUpperCase()}${n}`);
+var rows = _.zip(engSvgs, engStrings).map(v=>v.join(' ')).map(s=>`<p>${s}</p>`);
+console.log(_.chunk(rows, Math.ceil(rows.length/4)).map(v=>v.join('\n')).map(s=>`<div class="column">${s}</div>`).join('\n\n'))
+
+
+
+
 
 /*
 // See https://en.wikipedia.org/wiki/Web_colors
